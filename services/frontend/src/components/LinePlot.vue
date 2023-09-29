bf3818e4c166de499d868271d5634e42ad3cb756
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-09-29 12:53:35 +0200

    Part 5

--- services/frontend/src/components/LinePlot.vue
+++ services/frontend/src/components/LinePlot.vue
@@ -1,6 +1,10 @@
 <template>
-  <div style="height: 90vh">
-    <div id='myLinePlot' style="height: inherit">
+  <div>
+    <v-row align="center" justify="center" class="mt-1 mb-0">
+      <h3>Profit View</h3>
+    </v-row>
+    <div style="height: 90vh">
+      <div id='myLinePlot' style="height: inherit"></div>
     </div>
   </div>
 </template>
@@ -10,37 +14,37 @@ import Plotly from 'plotly.js/dist/plotly';
 export default {
   name: "LinePlot",
   data: () => ({
+    LinePlotData: { x: [], y: [] }
   }),
   mounted() {
-    this.drawScatterPlot()
+    this.fetchData()
   },
   methods: {
-    drawScatterPlot() {
+    async fetchData() {
+      // req URL to retrieve single company from backend
+      var reqUrl = 'http://127.0.0.1:5000/companies/1'
+      console.log("ReqURL " + reqUrl)
+      // await response and data
+      const response = await fetch(reqUrl)
+      const responseData = await response.json();
+      // transform data to usable by lineplot
+      responseData.profit.forEach((profit) => {
+        this.LinePlotData.x.push(profit.year)
+        this.LinePlotData.y.push(profit.value)
+      })
+      // draw the lineplot after the data is transformed
+      this.drawLinePlot()
+    },
+    drawLinePlot() {
       var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        mode: 'markers',
+        x: this.LinePlotData.x,
+        y: this.LinePlotData.y,
         type: 'scatter'
       };
-
-      var trace2 = {
-        x: [2, 3, 4, 5],
-        y: [16, 5, 11, 9],
-        mode: 'lines',
-        type: 'scatter'
-      };
-
-      var trace3 = {
-        x: [1, 2, 3, 4],
-        y: [12, 9, 15, 12],
-        mode: 'lines+markers',
-        type: 'scatter'
-      };
-
-      var data = [trace1, trace2, trace3];
-
-      Plotly.newPlot('myLinePlot', data);
-
+      var data = [trace1];
+      var layout = {}
+      var config = { responsive: true, displayModeBar: false }
+      Plotly.newPlot('myLinePlot', data, layout, config);
     }
   }
 }
